8c4e0a3296f3b55de328c4a70bdb61adbf59dd86
slysinema <ivan.sidorenko373@gmail.com>
2026-03-13 03:03:42 +0100

    refactor: wrapped code in concat_datasets() function refactor: added type hinting in a apply_smote() function

--- preparing/concat_datasets.py
+++ preparing/concat_datasets.py
@@ -1,61 +1,29 @@
 import pandas as pd
-from read_dataset import ReadDataset
 import numpy as np
+from preparing.read_dataset import ReadDataset
 
-#read all datasets
-data1 = ReadDataset("../datasets/raw/Benign-Monday-no-metadata.parquet").read_data()
-data2 = ReadDataset("../datasets/raw/Botnet-Friday-no-metadata.parquet").read_data()
-data3 = ReadDataset("../datasets/raw/Infiltration-Thursday-no-metadata.parquet").read_data()
+def concat_datasets() -> pd.DataFrame:
+    data1 = ReadDataset("../datasets/raw/Benign-Monday-no-metadata.parquet").read_data()
+    data2 = ReadDataset("../datasets/raw/Botnet-Friday-no-metadata.parquet").read_data()
+    data3 = ReadDataset("../datasets/raw/Infiltration-Thursday-no-metadata.parquet").read_data()
 
-#info about Bening dataset
-print("./datasets/raw/Benign-Monday-no-metadata.parquet")
-data1.info()
-print(data1.head())
-print("\n")
+    df = pd.concat([data1, data2, data3], ignore_index=True)
 
-#info about Botnets dataset
-print("./datasets/raw/Botnet-Friday-no-metadata.parquet")
-data2.info()
-print(data2.head())
-print("\n")
+    columns_to_keep = [
+        "Flow IAT Mean", "Flow IAT Std", "Flow IAT Max", "Flow IAT Min",
+        "Total Fwd Packets", "Total Backward Packets",
+        "Fwd Packets Length Total", "Bwd Packets Length Total",
+        "Packet Length Min", "Packet Length Max", "Packet Length Mean",
+        "Packet Length Std", "Packet Length Variance",
+        "Flow Bytes/s", "Flow Packets/s",
+        "Label"
+    ]
 
-#info about Infiltration dataset
-print("./datasets/raw/Infiltration-Thursday-no-metadata.parquet")
-data3.info()
-print(data3.head())
+    df = df[columns_to_keep]
+    df["y"] = (df["Label"] != "Benign").astype(int)
+    df = df.replace([np.inf, -np.inf], np.nan).dropna()
 
-#concat all datasets in one file result.parquet for ml
-df = pd.concat([data1, data2, data3], ignore_index=True)
-df.to_parquet("../datasets/result/full/full_dataset_before_clean.parquet")
+    numeric_cols = df.select_dtypes(include=[np.number]).columns
+    df = df[(df[numeric_cols] >= 0).all(axis=1)]
 
-#normalization result dataset based on the analysis from the bachelor's thesis
-#leave only 15 columns for further machine training
-columns_to_keep = [
-    "Flow IAT Mean", "Flow IAT Std", "Flow IAT Max", "Flow IAT Min",
-    "Total Fwd Packets", "Total Backward Packets",
-    "Fwd Packets Length Total", "Bwd Packets Length Total",
-    "Packet Length Min", "Packet Length Max", "Packet Length Mean",
-    "Packet Length Std", "Packet Length Variance",
-    "Flow Bytes/s", "Flow Packets/s",
-    "Label"
-]
-
-result = df
-result = result[columns_to_keep]
-result["y"] = (result["Label"] != "Benign").astype(int)
-
-print("full_dataset_before_clean.parquet")
-result.info()
-print(result.head())
-
-#cleaning full dataset(deleting rows with Infinity/NaN/Null columns)
-result = result.replace([np.inf, -np.inf], np.nan).dropna()
-
-#cleaning full dataset(deleting rows with negative columns
-numeric_cols = result.select_dtypes(include=[np.number]).columns
-result = result[(result[numeric_cols] >= 0).all(axis=1)]
-
-result.to_parquet("../datasets/result/full/full_dataset_after_clean.parquet")
-print("full_dataset_after_clean.parquet")
-result.info()
-
+    return df
